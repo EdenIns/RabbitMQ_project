a399fd38a23e75407b3f9ccb5d2fbcc33f2597fa
EdenIns <e.oudaya@outlook.com>
2025-05-20 15:46:38 +0200

    refactor: changer sendRequest.js

--- producer/sendRequest.js
+++ producer/sendRequest.js
@@ -2,29 +2,35 @@ const amqp = require("amqplib");
 const { randomInt } = require("crypto");
 
 const RABBITMQ_URL = "amqp://user:password@efrei20250519.hopto.org:5674";
-const QUEUE_NAME = "calcul_requests";
+const EXCHANGE_NAME = "calc_direct";
+
+const operations = ["add", "sub", "mul", "div"];
+
+function getRandomOp() {
+  const index = randomInt(operations.length);
+  return operations[index];
+}
 
 async function connectAndSend() {
   try {
     const connection = await amqp.connect(RABBITMQ_URL);
     const channel = await connection.createChannel();
 
-    await channel.assertQueue(QUEUE_NAME, { durable: false });
+    await channel.assertExchange(EXCHANGE_NAME, "direct", { durable: false });
 
     setInterval(() => {
       const n1 = randomInt(1, 100);
       const n2 = randomInt(1, 100);
+      const op = getRandomOp();
 
-      const message = {
-        n1,
-        n2,
-      };
+      const message = { n1, n2, op };
 
-      channel.sendToQueue(QUEUE_NAME, Buffer.from(JSON.stringify(message)));
-      console.log("Message sent :", message);
+      channel.publish(EXCHANGE_NAME, op, Buffer.from(JSON.stringify(message)));
+
+      console.log(`Sent to "${op}" :`, message);
     }, 5000);
   } catch (err) {
-    console.error("Error", err);
+    console.error("Error:", err);
   }
 }
 
